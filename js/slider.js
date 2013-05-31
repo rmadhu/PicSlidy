7a7155b76a6d473fd93114881fff3f04b1d3db64
rmadhu <ramalingam.madhu@gmail.com>
2013-05-31 14:23:45 +1000

    add namespace details

--- js/slider.js
+++ js/slider.js
@@ -5,23 +5,29 @@
  * Time: 6:45 PM
  * To change this template use File | Settings | File Templates.
  */
-var step = 0, canvas, context, images, interval, imageNames;
-window.addEventListener('keydown',this.handleArrowKeys,false);
 
-function fillCanvas(){
+//top-level namespace assigned to an object literal
+var picSlidy = {
+step:0,
+canvas:null,
+context:null,
+images:null,
+interval:null,
+imageNames: new Array(),
+
+fillCanvas: function(){
     canvas=document.getElementById("picCanvas");
     context = canvas.getContext("2d");
-
     //array of images to be showcased
     imageNames = ["i4.jpg","i5.jpg","i6.jpg"];
     images = new Array(imageNames.length);
-    for(k=0;k<imageNames.length;k++)   {
+    for(var k=0;k<imageNames.length;k++)   {
         images[k]  = new Image();
         images[k].src = "images/"+imageNames[k];
     }
 
     //loading first image
-    context.drawImage(images[step], 0, 0);
+    context.drawImage(images[this.step], 0, 0);
 
     //if there are more than one images then enable the 'next' button which is disabled by default
     if(images.length>1){
@@ -31,48 +37,49 @@ function fillCanvas(){
     }
 
     //start slide show
-    playSlideShow();
+    this.playSlideShow();
 
-}
+},
 
-function nextImage(){
+nextImage: function(){
     //if slide show is on then stops it
     if(interval) {
         window.clearInterval(interval);
     }
-    if(step<images.length-1){
-        step=step+1;
+    if(this.step<images.length-1){
+        this.step=this.step+1;
     }
-    if(step>0){
-        var disablePrevButton = document.getElementById("buttonBack");
-        disablePrevButton.disabled=false;
-        disablePrevButton.className="button enabled";
-    }
-    else //if(step>=images.length-1)
+    if(this.step>=images.length-1)
     {
         var disableNextButton = document.getElementById("buttonNext");
         disableNextButton.disabled=true;
         disableNextButton.className="button disabled";
     }
-    context.drawImage(images[step], 0, 0);
+    if(this.step>0){
+        var disablePrevButton = document.getElementById("buttonBack");
+        disablePrevButton.disabled=false;
+        disablePrevButton.className="button enabled";
+    }
 
-}
+    context.drawImage(images[this.step], 0, 0);
 
-function prevImage(){
+},
+
+prevImage: function(){
     //if slide show is on then stops it
     if(interval) {
         window.clearInterval(interval);
     }
-    if(step>0){
-        step=step-1;
+    if(this.step>0){
+        this.step=this.step-1;
     }
-    context.drawImage(images[step], 0, 0);
-    if(step>=1)
+    context.drawImage(images[this.step], 0, 0);
+    if(this.step>=1)
     {
         var disablePrevButton = document.getElementById("buttonBack");
         disablePrevButton.disabled=false;
         disablePrevButton.className="button enabled";
-        if(step<images.length-1)
+        if(this.step<images.length-1)
         {
             var disableNextButton = document.getElementById("buttonNext");
             disableNextButton.disabled=false;
@@ -84,9 +91,9 @@ function prevImage(){
         disablePrevButton.disabled=true;
         disablePrevButton.className="button disabled";
     }
-}
+},
 
-function handleArrowKeys(e){
+handleArrowKeys: function(e){
     var code = e.keyCode;
     //if left arrow is pressed, prevent default behavior, stop slide show and go to previous image
     if(code==37){
@@ -94,18 +101,18 @@ function handleArrowKeys(e){
         if(interval) {
             window.clearInterval(interval);
         }
-        prevImage();
+        picSlidy.prevImage();
     } else if(code==39){  //if right arrow is pressed, prevent default behavior, stop slide show and go to next image
         e.preventDefault();
         if(interval) {
             window.clearInterval(interval);
         }
-        nextImage();
+        picSlidy.nextImage();
     }
 
-}
+},
 
-function playSlideShow() {
+playSlideShow:function() {
     var i = -1;
 
     //image changes every 2 seconds until a button is button or left/right arrow keys are pressed
@@ -133,13 +140,15 @@ function playSlideShow() {
             disableNextButton.className="button disabled";
         }
         context.drawImage(images[i], 0, 0);
-        //to keep step up-to-date
-        step=i;
+        //to keep this.step up-to-date
+        this.step=i;
         //to keep the slide show running
         if(i==images.length-1)  {
             i=-1;
         }
     }, 2000);
 
-}
+    }
+} //object literal closing
+window.addEventListener('keydown',picSlidy.handleArrowKeys,false);
 
